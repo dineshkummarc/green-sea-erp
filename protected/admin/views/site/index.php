76fc05dc138d991764de9eed1a102d5d9a322186
yuanchensi <yuan1238y@sina.com>
2011-11-05 10:33:58 +0800

    修正模型orderType文件获取错误的问题 删除后台自动登录框弹出

--- protected/admin/views/site/index.php
+++ protected/admin/views/site/index.php
@@ -57,8 +57,6 @@
     <script type="text/javascript">
     $(function(){
     	DWZ.init("admin/dwz.frag.xml", {
-    		loginUrl:"login_dialog.html", loginTitle:"登录",	// 弹出登录对话框
-    		loginUrl:"login.html",	// 跳到登录页面
     		statusCode:{ok:200, error:300, timeout:301}, //【可选】
     		pageInfo:{pageNum:"pageNum", numPerPage:"numPerPage", orderField:"orderField", orderDirection:"orderDirection"}, //【可选】
     		debug:false,	// 调试模式 【true|false】
