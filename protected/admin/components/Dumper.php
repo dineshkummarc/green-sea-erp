2feffc2f6f8f5ca159ecc956e3b5e546aeba5f04
yuanchensi <yuan1238y@sina.com>
2011-11-06 21:24:12 +0800

    修正pageForm的url不带参数导致的页面刷新错误的问题 修正Dumper文件合并错误的问题

--- protected/admin/components/Dumper.php
+++ protected/admin/components/Dumper.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 class Dumper extends CVarDumper
 {
@@ -12,19 +11,4 @@ class Dumper extends CVarDumper
         return parent::dumpAsString($var, $depth, $highlight);
     }
 }
-=======
-<?php
-class Dumper extends CVarDumper
-{
-    public static function dump($var, $highlight = true, $depth = 10)
-    {
-        parent::dump($var, $depth, $highlight);
-    }
-
-    public static function dumpString($var, $highlight = true, $depth = 10)
-    {
-        return parent::dumpAsString($var, $depth, $highlight);
-    }
-}
->>>>>>> ee77750e3ad3c002b0b50fb19d8daba9222d91c4
 ?>
