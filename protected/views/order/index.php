f35f05917d51c318e3afa19ddf5f1a44a504d4be
yuanchensi <yuan1238y@sina.com>
2011-11-10 22:20:02 +0800

    前台订单列表，删除查询及排序功能

--- protected/views/order/index.php
+++ protected/views/order/index.php
@@ -1,18 +1,11 @@
-<div style="margin-bottom: 10px; margin-left: 10px;">
-    <form action="" method="get">
-        <input type="hidden" name="r" value="<?php echo $this->id.'/'.$this->action->id; ?>" />
-        物流单号：<input type="text" name="logistics" value="<?php echo isset($_GET['logistics']) ? $_GET['logistics'] : '' ?>" />
-        <input type="submit" value="查询" />
-    </form>
-</div>
 <table class="list" cellpadding="0" cellspacing="0" style="margin-bottom: 10px;">
     <tr class="head">
         <th width="10%">客户名</th>
         <th width="10%">旺旺号</th>
         <th width="8%">订单号</th>
         <th width="10%" align="center">合同金额</th>
-        <th width="20%" align="center"><a href="<?php echo $this->createUrl('', array('sort'=>'time')); ?>" title="按下单时间排序">下单时间</a></th>
-        <th width=""><a href="<?php echo $this->createUrl('', array('sort'=>'status')); ?>" title="按状态排序">当前状态</a></th>
+        <th width="20%" align="center">下单时间</th>
+        <th width="">当前状态</th>
         <th width="10%" align="center">是否跟拍</th>
         <th width="18%" align="center">操作</th>
     </tr>
