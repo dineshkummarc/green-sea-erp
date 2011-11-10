2ed563e27f5f68a4413028618e0110cd2e404609
yuanchensi <yuan1238y@sina.com>
2011-11-10 09:41:17 +0800

    删除永不到的view 用户添加时，添加收货地址有问题，删除改模块

--- protected/admin/controllers/UserController.php
+++ protected/admin/controllers/UserController.php
@@ -12,11 +12,11 @@ class UserController extends Controller
 
         $user = new User;
         $sheng = Area::model()->findAreaByLevel();
-    	$shi = Area::model()->findAreaByLevel(2);
-    	$qu = Area::model()->findAreaByLevel(3);
-   		$area['sheng'] = $sheng;
-    	$area['shi'] = $shi;
-    	$area['qu'] = $qu;
+        $shi = Area::model()->findAreaByLevel(2);
+        $qu = Area::model()->findAreaByLevel(3);
+           $area['sheng'] = $sheng;
+        $area['shi'] = $shi;
+        $area['qu'] = $qu;
         $criteria = $user->dbCriteria;
         $criteria->order = "create_time DESC";
         if (!empty($params['name']))
@@ -25,8 +25,8 @@ class UserController extends Controller
             $criteria->addCondition('mobile_phone = \'' . $params['phone'] . '\'');
         if (!empty($params['mail']))
             $criteria->addSearchCondition('email', $params['mail']);
-		if(!empty($params['id']))
-			$criteria->addSearchCondition('id',intval(substr($params['id'], 1)));
+        if(!empty($params['id']))
+            $criteria->addSearchCondition('id',intval(substr($params['id'], 1)));
         $count = $user->count($criteria);
         $pages = new CPagination($count);
         $pages->currentPage = empty($pageNum) ? 0 : $pageNum - 1;
@@ -40,120 +40,49 @@ class UserController extends Controller
      * 修改/添加用户
      * @param integer $id
      */
-	public  function  actionEdit($id=null)
-	{
-			$user = new User;
-			// 获取省份信息
-	    	$sheng = Area::model()->findAreaByLevel();
-	    	$shi = Area::model()->findAreaByLevel(2);
-	    	$qu = Area::model()->findAreaByLevel(3);
-   			$area['sheng'] = $sheng;
-	    	$area['shi'] = $shi;
-	    	$area['qu'] = $qu;
-			$areas = Area::model()->cache()->findAll(array('condition'=>'parent_id = 0'));
-			if (!empty($id))
-			{
-					$user = $user->cache()->findByPk($id);
-					$receiver = UserReceive::model()->findByPk($user->receive_id);
-					if (empty($receiver))  $receiver = new UserReceive;
-
-					if ($receiver->area_id == 0)
-						$area_list = null;
-					else
-						$area_list = Area::getAreaLevelAll($receiver->area_id);
-			}
-			else
-			{
-				$area_list = null;
-				 $receiver = new UserReceive;
-			}
-	        if (isset($_POST['Form']))
-        	{
-        		    if($_POST['Form']['area_1']=="all")
-        		    {
-        		    		$message="请选择省份";
-        		    		$this->error($message);
-        		    }
-        	      	if($_POST['Form']['area_2']=="all")
-        		    {
-        		    		$message="请选择市区";
-        		    		$this->error($message);
-        		    }
-        	        if($_POST['Form']['area_id']=="all")
-        		    {
-        		    		$message="请选择具体地区";
-        		    		$this->error($message);
-        		    }
-        			if (!empty($_POST['Form']['id']))
-        				$user = $user->cache()->findByPk($_POST['Form']['id']);
-        		    if (!empty($_POST['Form']['id']) && trim($_POST['Form']['password']) == "")
-                			$_POST['Form']['password'] = $user->password;
-            		else
-                			$_POST['Form']['password'] = md5(trim($_POST['Form']['password']));
-	        		$phone1 = trim($_POST['Form']['phone-1']);
-	                $phone2 = trim($_POST['Form']['phone-2']);
-	                $phone3 = trim($_POST['Form']['phone-3']);
-                    unset($_POST['Form']['phone-1'], $_POST['Form']['phone-2'], $_POST['Form']['phone-3']);
-                	if (empty($phone1) && empty($phone2))
-                    		$_POST['Form']['phone'] = "";
-                	else
-                    		$_POST['Form']['phone'] = $phone1 . "-" . $phone2;
-                	if (!empty($phone) && empty($phone3))
-                	{
-                    		 $_POST['Form']['phone'] .= "-" . $phone3;
-                	}
-                	$_POST['Form']['phone'] = trim($_POST['Form']['phone']);
-                	$user->attributes = $_POST['Form'];
-                	$user->first = 1;
-                	$user->admin_id=Yii::app()->user->id;
-					$user->accumulation_price = 0;
-					$user->receive_id = 0;
-					$user->receive_count = 0;
-					$user->next_order = 1;
-					$user->login_time = 0;
-					$user->last_ip = 0;
-					$user->create_time = Yii::app()->params['timestamp'];
-					if($user->save())
-					{
-
-								$receiver->attributes = $_POST['Form'];
-								$receiver->user_id=$user->id;
-								$receiver->receive_name=$user->name;
-								if($receiver->save())
-								{
-										$user->receive_count+1;
-										$user->receive_id=$receiver->id;
-										$user->save();
-										 if (!empty($_POST['Form']['id']))
-										 {
-										 		$message="修改成功";
-										 		$this->success($message,array('navTabId'=>'user-index') );
-										 }
-		 								else
-		 								{
-					 							$message="添加成功";
-												$this->success($message,array('navTabId'=>'user-index') );
-		 								}
-								}
-								else
-								{
-										$error = array_shift($receiver->getErrors());
-										 if (!empty($_POST['Form']['id']))  $message="修改失败".$error[0];
-										 			else
-										 					$message="添加失败";
-										$this->error($message);
-								}
-
-					}
-					else
-					{
-								$error = array_shift($user->getErrors());
-								$message="修改失败".$error[0];
-								$this->error($message);
-					}
-        	}
-        		$this->render("edit", array('user'=>$user,'area'=>$area,'areas'=>$areas,'area_list'=>$area_list,'receiver'=>$receiver));
-	}
+    public  function  actionEdit($id=null)
+    {
+        $user = new User;
+        if (isset($_POST['Form']))
+        {
+            if (!empty($_POST['Form']['id']))
+                $user = $user->cache()->findByPk($_POST['Form']['id']);
+            if (!empty($_POST['Form']['id']) && trim($_POST['Form']['password']) == "")
+                    $_POST['Form']['password'] = $user->password;
+            else
+                    $_POST['Form']['password'] = md5(trim($_POST['Form']['password']));
+            $user->attributes = $_POST['Form'];
+            $user->first = 1;
+            $user->admin_id=Yii::app()->user->id;
+            $user->accumulation_price = 0;
+            $user->receive_id = 0;
+            $user->receive_count = 0;
+            $user->next_order = 1;
+            $user->login_time = 0;
+            $user->last_ip = 0;
+            $user->create_time = Yii::app()->params['timestamp'];
+            if($user->save())
+            {
+                if (!empty($_POST['Form']['id']))
+                 {
+                     $message="修改成功";
+                     $this->success($message,array('navTabId'=>'user-index') );
+                 }
+                else
+                {
+                    $message="添加成功";
+                    $this->success($message,array('navTabId'=>'user-index') );
+                }
+            }
+            else
+            {
+                $error = array_shift($user->getErrors());
+                $message="修改失败".$error[0];
+                $this->error($message);
+            }
+        }
+        $this->render("edit", array('user'=>$user));
+    }
 
 
 
@@ -191,28 +120,28 @@ class UserController extends Controller
     }
     public function actionArea($type)
     {
-		if ($type == 'all')
-		{
-			$areas =  Area::model()->cache()->findAll(array('condition'=>'parent_id = 0'));
-			echo json_encode($this->AreaFormat($areas));
-//			print_r ($this->AreaFormat($areas));
-			return;
-		}else{
-			$areas =  Area::model()->cache()->findAll(array('condition'=>'parent_id = '.$type));
-			echo json_encode($this->AreaFormat($areas));
-//			print_r ($this->AreaFormat($areas));
-			return;
-		}
+        if ($type == 'all')
+        {
+            $areas =  Area::model()->cache()->findAll(array('condition'=>'parent_id = 0'));
+            echo json_encode($this->AreaFormat($areas));
+//            print_r ($this->AreaFormat($areas));
+            return;
+        }else{
+            $areas =  Area::model()->cache()->findAll(array('condition'=>'parent_id = '.$type));
+            echo json_encode($this->AreaFormat($areas));
+//            print_r ($this->AreaFormat($areas));
+            return;
+        }
     }
     public function AreaFormat($areas)
     {
-    	$list = array();
-		foreach ($areas as $area)
-		{
-			$list[$area->id][0] = $area->id;
-			$list[$area->id][1] = $area->name;
-		}
-		return $list;
+        $list = array();
+        foreach ($areas as $area)
+        {
+            $list[$area->id][0] = $area->id;
+            $list[$area->id][1] = $area->name;
+        }
+        return $list;
     }
 }
 ?>
--- protected/admin/views/user/edit.php
+++ protected/admin/views/user/edit.php
@@ -1,78 +1,6 @@
-
 <div class="pageContent">
     <form action="<?php echo $this->createUrl(''); ?>"  class="pageFormrequiredd-validate" onsubmit="return validateCallback(this, dialogAjaxDone);" method="post">
         <div class="pageFormContent" layoutH="60">
-            <div class="unit">
-                <label>收货人姓名</label>
-                <span class="unit"><input type="text" name="Form[receive_name]" class="required" value="<?php  echo!empty($receiver->receive_name)?$receiver->receive_name:null   ?>" alt="收货人姓名不能为空" /></span>
-            </div>
-
-            <div class="unit" >
-                <label>所在省:</label>
-	            <select class="combox required" name="Form[area_1]" ref="w_combox_city" default="5" refUrl="<?php echo $this->createUrl('user/area');?>&type={value}">
-
-					<?php if ($area_list != null):?>
-						<?php foreach ($area_list['1'] as $val):?>
-						<option value="<?php echo $val['id']?>" <?php if ($val['id'] == $area_list['default']['1'])echo "selected";?>><?php echo $val['name']?></option>
-						<?php endforeach;?>
-					<?php else:?>
-						<option value="all">所有省市</option>
-						<?php foreach ($areas as $val):?>
-						<option value="<?php echo $val->id?>" ><?php echo $val->name?></option>
-						<?php endforeach;?>
-					<?php endif;?>
-				</select>
-            </div>
-
-            <div class="unit">
-	            <label>所在市:</label>
-				<select class="combox" name="Form[area_2]" id="w_combox_city" ref="w_combox_area" refUrl="<?php echo $this->createUrl('user/area');?>&type={value}">
-					<?php if ($area_list != null):?>
-						<?php foreach ($area_list['2'] as $val):?>
-						<option value="<?php echo $val['id']?>" <?php if ($val['id'] == $area_list['default']['2'])echo "selected";?>><?php echo $val['name']?></option>
-						<?php endforeach;?>
-					<?php else:?>
-					<option value="all">所有城市</option>
-					<?php endif;?>
-				</select>
-            </div>
-
-            <div class="unit">
-	            <label>所在区:</label>
-				<select class="combox required" name="Form[area_id]" id="w_combox_area">
-					<?php if ($area_list != null):?>
-						<?php foreach ($area_list['3'] as $val):?>
-						<option value="<?php echo $val['id']?>" <?php if ($val['id'] == $area_list['default']['3'])echo "selected";?>><?php echo $val['name']?></option>
-						<?php endforeach;?>
-					<?php else:?>
-					<option value="all">所有区县</option>
-					<?php endif;?>
-				</select>
-            </div>
-
-            <div class="unit">
-	            <label>电话号码:</label>
-	            <?php $phone = explode('-', $receiver->phone); ?>
-	            <input type="text" name="Form[phone-1]" class="required" style="width: 50px" value="<?php echo empty($phone[1]) ? '' : $phone[0] ?>"/>
-	            <span style="float: left;">-</span>
-	            <input type="text" name="Form[phone-2]" class="required" style="width: 120px" value="<?php echo empty($phone[1]) ? $phone[0] : $phone[1] ?>"/>
-	            <span style="float: left;">-</span>
-	            <input type="text" name="Form[phone-3]" style="width: 50px" value="<?php echo empty($phone[2]) ? '' : $phone[2] ?>"/>
-	            <span class="form-require">*</span>
-	            <span class="form-prompt">区号-电话号码-分机</span>
-        </div>
-
-
-          <div class="unit">
-	            <label>详细地址:</label>
-	            <textarea name="Form[street]" class="required" style="width: 300px; height: 50px;" ><?php  echo!empty($receiver->	street)?$receiver->	street:null   ?></textarea>
-	            <span class="form-prompt">不需要重复填写省/市/区</span>
-        	</div>
-
-            <div class="unit" >
-            <label>邮政编码:</label>
-            <input type="text" name="Form[postalcode]" class="required" style="width: 80px"  value="<?php  echo!empty($receiver->postalcode)?$receiver->postalcode:null   ?>"   />
-            </div>
             <input type="hidden" name="Form[id]" value="<?php echo $user->id; ?>" />
             <?php if (!empty($user->id)): ?>
             <div class="unit">
@@ -118,7 +46,7 @@
                 <input type="text" name="Form[score]" value="<?php echo $user->score; ?>" class="required number" alt="积分不能为空，为数字" />
             </div>
         </div>
-             <div class="formBar">
+            <div class="formBar">
             <ul>
                 <li><div class="buttonActive"><div class="buttonContent"><button type="submit">确定 </button></div></div></li>
                 <li><div class="button"><div class="buttonContent"><button type="button" class="close">取消</button></div></div></li>
