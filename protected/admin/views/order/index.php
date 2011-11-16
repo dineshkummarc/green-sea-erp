6f5aa9de182ed3ed07dc72cc262ae303f1e6c348
chenlong <756618802@qq.com>
2011-11-16 09:49:21 +0800

    up 订单列表

--- protected/admin/views/order/index.php
+++ protected/admin/views/order/index.php
@@ -14,7 +14,7 @@ function status(val)
 }
 </script>
 <?php $this->widget('widget.Search', array(
-    'panleStyle'=>'width: 600px; height: 50px;',
+    'panleStyle'=>'width: 900px; height: 50px;',
     'searchCondition'=>array(
         '订单号：'=>array('type'=>'text', 'name'=>'params[sn]', 'defaultValue'=>empty($params['sn']) ? '' : $params['sn'], 'alt'=>'支持模糊搜索'),
         '运单号：'=>array('type'=>'text', 'name'=>'params[logistics_sn]', 'defaultValue'=>empty($params['logistics_sn']) ? '' : $params['logistics_sn'], 'alt'=>'支持模糊搜索'),
