a052cbeefe1a8a3f1ddd31469d74cc85c995fbc0
yuanchensi <yuan1238y@sina.com>
2011-11-16 13:58:59 +0800

    后台框架JS文件压缩

--- protected/admin/views/site/index.php
+++ protected/admin/views/site/index.php
@@ -6,8 +6,6 @@
 
     <link href="admin/themes/default/style.css" rel="stylesheet" type="text/css" />
 	<link href="admin/themes/css/core.css" rel="stylesheet" type="text/css" />
-	<!-- <link href="uploadify/css/uploadify.css" rel="stylesheet" type="text/css" /> 更换编辑器--删除旧的编辑器样式表 -->
-	<!-- 引用新的编辑器CSS -->
 	<link href="admin/keditor/themes/default/default.css" rel="stylesheet" type="text/css" />
 	<link href="admin/keditor/plugins/code/prettify.css" rel="stylesheet" type="text/css" />
 
@@ -17,54 +15,12 @@
 	<!--[if IE]>
 	<link href="admin/themes/css/ieHack.css" rel="stylesheet" type="text/css" />
 	<![endif]-->
-
-	<script src="admin/js/speedup.js" type="text/javascript"></script>
 	<script src="admin/js/jquery.js" type="text/javascript"></script>
-	<script src="admin/js/jquery.cookie.js" type="text/javascript"></script>
-	<script src="admin/js/jquery.validate.js" type="text/javascript"></script>
-	<script src="admin/js/jquery.bgiframe.js" type="text/javascript"></script>
-	<!-- <script src="xheditor/xheditor-1.1.9-zh-cn.min.js" type="text/javascript"></script>删除旧版编辑器JS -->
-	<!-- 引用新的编辑器JS -->
+    <script src="admin/js/dwz-min.js" type="text/javascript"></script>
     <script src="admin/keditor/kindeditor.js" type="text/javascript"></script>
     <script src="admin/keditor/lang/zh_CN.js" type="text/javascript"></script>
 	<script src="admin/uploadify/scripts/swfobject.js" type="text/javascript"></script>
 	<script src="admin/uploadify/scripts/jquery.uploadify.v2.1.0.js" type="text/javascript"></script>
-
-	<script src="admin/js/dwz.core.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.util.date.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.validate.method.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.regional.zh.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.barDrag.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.drag.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.tree.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.accordion.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.ui.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.theme.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.switchEnv.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.alertMsg.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.contextmenu.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.navTab.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.tab.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.resize.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.dialog.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.dialogDrag.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.cssTable.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.stable.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.taskBar.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.ajax.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.pagination.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.database.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.datepicker.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.effects.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.panel.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.checkbox.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.history.js" type="text/javascript"></script>
-	<script src="admin/js/dwz.combox.js" type="text/javascript"></script>
-
-	<!--
-	<script src="bin/dwz.min.js" type="text/javascript"></script>
-	-->
-	<script src="admin/js/dwz.regional.zh.js" type="text/javascript"></script>
 
     <!-- 引用自定义js -->
     <script src="admin/js/admin.js" type="text/javascript"></script>
@@ -76,7 +32,6 @@
     		debug:false,	// 调试模式 【true|false】
     		callback:function(){
     			initEnv();
-    			$("#themeList").theme({themeBase:"themes"}); // themeBase 相对于index页面的主题base路径
     		}
     	});
     });
